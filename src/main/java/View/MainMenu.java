e3f992e7d4e7bf2a9917af601f5f91e3095c2970
francisco marques <1212081@isep.ipp.pt>
2023-04-19 18:55:45 +0100

    implementação da verificação de autenticação (para não pedir repetidamente o login)

--- src/main/java/View/MainMenu.java
+++ src/main/java/View/MainMenu.java
@@ -19,6 +19,7 @@ public class MainMenu {
     private BookMenu bookMenu;
     private ReservationMenu reservationMenu;
     private Authentication authentication;
+    private boolean authenticated; 
 
     public MainMenu() {
         scanner = new Scanner(System.in);
@@ -44,68 +45,69 @@ public class MainMenu {
         bookMenu = new BookMenu(this);
         reservationMenu = new ReservationMenu(this, memberController, bookController, reservationController);
         authentication = new Authentication();
+        authenticated = false; 
     }
 
     public void displayMenu() {
-        System.out.println("===================================");
-        System.out.println("| Bem-vindo à biblioteca!        |");
-        System.out.println("| Faça login para acessar o menu |");
-        System.out.println("===================================");
-        boolean authenticated = authentication.login();
-
-        if (authenticated) {
+        if (!authenticated) { 
             System.out.println("===================================");
-            System.out.println("| Selecione uma opção:           |");
+            System.out.println("| Bem-vindo à biblioteca!        |");
+            System.out.println("| Faça login para acessar o menu |");
             System.out.println("===================================");
-            System.out.println("| 1 - Gestão de reservas         |");
-            System.out.println("| 2 - Gestão de autores          |");
-            System.out.println("| 3 - Gestão de bibliotecários   |");
-            System.out.println("| 4 - Gestão de membros          |");
-            System.out.println("| 5 - Gestão de editoras         |");
-            System.out.println("| 6 - Gestão de categorias       |");
-            System.out.println("| 7 - Gestão de faixa etária     |");
-            System.out.println("| 8 - Gestão de livros           |");
-            System.out.println("| 0 - Sair                       |");
-            System.out.println("===================================");
-
-            int option = scanner.nextInt();
-            scanner.nextLine();
-
-            switch (option) {
-                case 1:
-                    reservationMenu.start();
-                    break;
-                case 2:
-                    authorMenu.start();
-                    break;
-                case 3:
-                    librarianMenu.start();
-                    break;
-                case 4:
-                    memberMenu.start();
-                    break;
-                case 5:
-                    publisherMenu.start();
-                    break;
-                case 6:
-                    categoryMenu.start();
-                    break;
-                case 7:
-                    ageRangeMenu.start();
-                    break;
-                case 8:
-                    bookMenu.displayMenu();
-                    break;
-                case 0:
-                    System.out.println("Até logo!");
-                    System.exit(0);
-                    break;
-                default:
-                    System.out.println("Opção inválida. Tente novamente.");
-                    break;
-            }
-
-            displayMenu();
+            authenticated = authentication.login(); 
         }
+
+        System.out.println("===================================");
+        System.out.println("| Selecione uma opção:           |");
+        System.out.println("===================================");
+        System.out.println("| 1 - Gestão de reservas         |");
+        System.out.println("| 2 - Gestão de autores          |");
+        System.out.println("| 3 - Gestão de bibliotecários   |");
+        System.out.println("| 4 - Gestão de membros          |");
+        System.out.println("| 5 - Gestão de editoras         |");
+        System.out.println("| 6 - Gestão de categorias       |");
+        System.out.println("| 7 - Gestão de faixa etária     |");
+        System.out.println("| 8 - Gestão de livros           |");
+        System.out.println("| 0 - Sair                       |");
+        System.out.println("===================================");
+
+        int option = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (option) {
+            case 1:
+                reservationMenu.start();
+                break;
+            case 2:
+                authorMenu.start();
+                break;
+            case 3:
+                librarianMenu.start();
+                break;
+            case 4:
+                memberMenu.start();
+                break;
+            case 5:
+                publisherMenu.start();
+                break;
+            case 6:
+                categoryMenu.start();
+                break;
+            case 7:
+                ageRangeMenu.start();
+                break;
+            case 8:
+                bookMenu.displayMenu();
+                break;
+            case 0:
+                System.out.println("Até logo!");
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Opção inválida. Tente novamente.");
+                break;
+        }
+
+        displayMenu();
     }
 }
